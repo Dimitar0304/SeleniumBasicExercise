e4fe299c3fd2f51a6d29be0fbed2472bc45140b6
Димитър Банчев <114139864+Dimitar0304@users.noreply.github.com>
2024-10-10 13:41:41 +0300

    some modify

--- TestProject1/obj/Debug/net6.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net6.0/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d58f115e16471085f22469587c3ca71c1c71ce0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5ac703d9d9bc8ce936cd1cff8230654f9063928")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
